f36ffa41f0114c6364ddeab8f675e0b7217163fd
takumi <takumidiary.0927@gmail.com>
2023-10-26 22:09:54 +0900

    binary search & add type ll

---
+++ binary-search/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, init, n) for (int i = init; i < (int)(n); i++)
+
+int main()
+{
+	
+}
--- main.cpp
+++ main.cpp
@@ -2,32 +2,27 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define repi(i, init, n) for (int i = init; i < (int)(n); i++)
+using ll = long long;
 
 int main()
 {
-    long long n,m; cin >> n >> m;
-    vector<long long> a(m);
-    rep(i,m) cin >> a[i];
-    vector<long long> dp(n+1, 0);
-    vector<bool> j(n+1, false);
-    long long s = 1e9 + 7;
-    dp[0] = 1, dp[1] = 1;
+  ll x, k, d;
+  cin >> x >> k >> d;
+  x = abs(x);
 
-    rep(i,m) {
-        j[a[i]] = true;
-    }
+  ll ans;
+  if (k <= x / d)
+  {
+    ans = x - k * d;
+  }
+  else
+  {
+    k -= x / d;
+    x -= x / d * d;
+    if (k % 2 == 1)
+      x -= d;
+    ans = abs(x);
+  }
 
-    if (j[1]) dp[1] = 0;
-    if (j[0]) {
-        cout << 0 << endl;
-        return 0;
-    }
-    repi(i, 2, n+1) {
-        if (j[i]) {
-            continue;
-        }
-        dp[i] = dp[i-1] + dp[i-2];
-        dp[i] %= s;
-    }
-    cout << dp[n] << endl;
+  cout << ans << endl;
 }
